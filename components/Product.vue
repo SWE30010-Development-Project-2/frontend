718548afb601782e248b63d96afda912ec8228fc
Scott97 <scottbarnard5@gmail.com>
2019-09-16 12:18:03 +1000

    Added Product and addsalesrecord and fixed linting

---
+++ components/Product.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-col>
+    <b-card
+      :title="name"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2"
+    />
+  </b-col>
+</template>
+
+<script>
+module.exports = {
+  props: ['name']
+}
+</script>
---
+++ pages/AddSalesRecord.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <h1 class="title">
+          Add Sales Record Page
+        </h1>
+        <b-form-input :type="search" placeholder="Search for Product" />
+      </b-col>
+    </b-row>
+    <b-row>
+      <product name="Product 1" />
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import Product from '~/components/Product.vue'
+
+export default {
+  components: {
+    Product
+  }
+}
+</script>
