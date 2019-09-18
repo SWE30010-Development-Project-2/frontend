d8ca898a42142205e9442ffbc763bf4d4374a9ad
unknown <nick.d.laverty@gmail.com>
2019-09-18 22:36:49 +1000

    Displaying UI finished

--- pages/DisplaySalesRecord.vue
+++ pages/DisplaySalesRecord.vue
@@ -1,6 +1,10 @@
+<script src="https://unpkg.com/vue@2.5.17/dist/vue.min.js"></script>
+<script src="https://unpkg.com/babel-polyfill@latest/dist/polyfill.min.js"></script>
+<script src="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.js"></script>
+
 <template>
   <div>
-    <navbar title="Add Sales Record Page" />
+    <navbar title="Add Sales Record Psales" />
     <div class="links">
       <a
         href=""
@@ -24,60 +28,217 @@
         Past year
       </a>
     </div>
-    <div id="table">
-      <input id="Input" type="text" onkeyup="myFunction()" placeholder="Search for names..">
-      <b-table striped :items="items" :fields="fields">
-        <b-table-column custom-key="actions">
-          <button class="button is-small is-light" @click="edit(props.row)">
-            <b-icon icon="edit" size="is-small" />
-          </button>
-        </b-table-column>
+
+    <b-container fluid>
+      <!-- User Interface controls -->
+      <b-row>
+        <b-col lg="6" class="my-1">
+          <b-form-group
+            label="Sort"
+            label-cols-sm="3"
+            label-align-sm="right"
+            label-size="sm"
+            label-for="sortBySelect"
+            class="mb-0"
+          >
+            <b-input-group size="sm">
+              <b-form-select id="sortBySelect" v-model="sortBy" :options="sortOptions" class="w-75">
+                <template v-slot:first>
+                  <option value="">
+                    -- none --
+                  </option>
+                </template>
+              </b-form-select>
+              <b-form-select v-model="sortDesc" size="sm" :disabled="!sortBy" class="w-25">
+                <option :value="false">
+                  Asc
+                </option>
+                <option :value="true">
+                  Desc
+                </option>
+              </b-form-select>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+
+        <b-col lg="6" class="my-1">
+          <b-form-group
+            label="Filter"
+            label-cols-sm="3"
+            label-align-sm="right"
+            label-size="sm"
+            label-for="filterInput"
+            class="mb-0"
+          >
+            <b-input-group size="sm">
+              <b-form-input
+                id="filterInput"
+                v-model="filter"
+                type="search"
+                placeholder="Type to Search"
+              />
+              <b-input-group-append>
+                <b-button :disabled="!filter" @click="filter = ''">
+                  Clear
+                </b-button>
+              </b-input-group-append>
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+
+        <b-col sm="5" md="6" class="my-1">
+          <b-form-group
+            label="Per psales"
+            label-cols-sm="6"
+            label-cols-md="4"
+            label-cols-lg="3"
+            label-align-sm="right"
+            label-size="sm"
+            label-for="perPsalesSelect"
+            class="mb-0"
+          >
+            <b-form-select
+              id="perPsalesSelect"
+              v-model="perPsales"
+              size="sm"
+              :options="psalesOptions"
+            />
+          </b-form-group>
+        </b-col>
+
+        <b-col sm="7" md="6" class="my-1">
+          <b-pagination
+            v-model="currentPsales"
+            :total-rows="totalRows"
+            :per-psales="perPsales"
+            align="fill"
+            size="sm"
+            class="my-0"
+          />
+        </b-col>
+      </b-row>
+
+      <!-- Main table element -->
+      <b-table
+        show-empty
+        small
+        stacked="md"
+        :items="items"
+        :fields="fields"
+        :current-psales="currentPsales"
+        :per-psales="perPsales"
+        :filter="filter"
+        :filter-included-fields="filterOn"
+        :sort-by.sync="sortBy"
+        :sort-desc.sync="sortDesc"
+        :sort-direction="sortDirection"
+        @filtered="onFiltered"
+      >
+        <template v-slot:cell(actions)="row">
+          <b-button size="sm" class="mr-1" @click="info(row.item, row.index, $event.target)">
+            Edit
+          </b-button>
+          <b-button size="sm" @click="row.toggleDetails">
+            Delete
+          </b-button>
+        </template>
+
+        <template v-slot:row-details="row">
+          <b-card>
+            <ul>
+              <li v-for="(value, key) in row.item" :key="key">
+                {{ key }}: {{ value }}
+              </li>
+            </ul>
+          </b-card>
+        </template>
       </b-table>
-    </div>
+
+      <!-- Info modal -->
+      <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
+        <pre>{{ infoModal.content }}</pre>
+      </b-modal>
+    </b-container>
   </div>
 </template>
 
 <script>
-import Navbar from '~/components/Navbar.vue'
-
-export default {
-  components: {
-    Navbar
-  },
-  data () {
-    return {
-      // changes how the fields of the tables interact
-      fields: [
-        {
-          key: 'Product_Name',
-          sortable: true
-        },
-        {
-          key: 'Sales_No',
-          sortable: false
-        },
-        {
-          key: 'actions',
-          sortable: false
+  export default {
+    data() {
+      return {
+        items: [
+          {  sales: 40, name: 'Xanax' },
+          {  sales: 21, name: 'Cocaine' },
+          { sales: 9,name: 'Glucophage'},
+          {  sales: 89, name: 'GHB' },
+          {  sales: 38, name: 'Vicodin' },
+          {  sales: 27, name: 'Lipitor' },
+          {  sales: 40, name: 'Zofran' },
+          {sales: 87,name: 'Panadol' },
+          {  sales: 26, name: 'Amoxil' },
+          {  sales: 22, name: 'Delasone' },
+          {  sales: 38, name: 'Neurontin' },
+          {  sales: 29, name: 'Prinivil' }
+        ],
+        fields: [
+          { key: 'name', label: 'Product Name', sortable: true, sortDirection: 'desc' },
+          { key: 'sales', label: 'Sales No', sortable: true, class: 'text-center' },
+          { key: 'actions', label: 'Actions' }
+        ],
+        totalRows: 1,
+        currentPsales: 1,
+        perPsales: 5,
+        psalesOptions: [5, 10, 15],
+        sortBy: '',
+        sortDesc: false,
+        sortDirection: 'asc',
+        filter: null,
+        filterOn: [],
+        infoModal: {
+          id: 'info-modal',
+          title: '',
+          content: ''
         }
-      ],
-      // contents of the table am not sure how to add edit button into this
-      items: [
-        { Product_Name: 'Xanax', Sales_No: '6' },
-        { Product_Name: 'Paracetemol', Sales_No: '6', Edit: 'OOF' },
-        { Product_Name: 'Dug9', Sales_No: '6', Edit: 'OOF' },
-        { Product_Name: 'Drug7', Sales_No: '6', Edit: 'OOF' }
-      ]
+      }
+    },
+    computed: {
+      sortOptions() {
+        // Create an options list from our fields
+        return this.fields
+          .filter(f => f.sortable)
+          .map(f => {
+            return { text: f.label, value: f.key }
+          })
+      }
+    },
+    mounted() {
+      // Set the initial number of items
+      this.totalRows = this.items.length
+    },
+    methods: {
+      info(item, index, button) {
+        this.infoModal.title = `Row index: ${index}`
+        this.infoModal.content = JSON.stringify(item, null, 2)
+        this.$root.$emit('bv::show::modal', this.infoModal.id, button)
+      },
+      resetInfoModal() {
+        this.infoModal.title = ''
+        this.infoModal.content = ''
+      },
+      onFiltered(filteredItems) {
+        // Trigger pagination to update the number of buttons/psaless due to filtering
+        this.totalRows = filteredItems.length
+        this.currentPsales = 1
+      }
     }
   }
-}
 </script>
 
 <style>
 #Input {
-    background-image: url('/css/searchicon.png'); /* Add a search icon to input */
+    background-imsales: url('/css/searchicon.png'); /* Add a search icon to input */
   background-position: 10px 12px; /* Position the search icon */
-  background-repeat: no-repeat; /* Do not repeat the icon image */
+  background-repeat: no-repeat; /* Do not repeat the icon imsales */
   width: 50%; /* Full-width */
   font-size: 16px; /* Increase font-size */
   padding: 12px 20px 12px 40px; /* Add some padding */
