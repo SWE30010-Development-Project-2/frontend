9710131152ec9b68f2b1ad2d0e746dd714188590
Scott97 <scottbarnard5@gmail.com>
2019-09-18 17:04:15 +1000

    Made the add sales record page interactive

--- components/AddedItem.vue
+++ components/AddedItem.vue
@@ -1,26 +1,31 @@
 <template>
   <b-row class="pb-1 pt-1 align-items-center">
-    <b-col cols="5">
+    <b-col cols="4">
       {{ quantity }} x {{ name }}
     </b-col>
     <b-col cols="4">
-      <number v-model="quantity" />
+      <number v-model="quantity" @input="$emit('update-qty',quantity)" />
     </b-col>
-    <b-col cols="3" class="text-right">
-      <b-button variant="outline-danger" class="w-100">
-        Remove
+    <b-col cols="4" class="text-right">
+      <b-button variant="outline-danger" class="w-100 text-nowrap" @click="$emit('deleteItem',index)">
+        <font-awesome-icon icon="trash-alt" /> Remove
       </b-button>
     </b-col>
   </b-row>
 </template>
 
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Number from '~/components/NumberPlusMinus.vue'
+library.add(faTrashAlt)
 
 export default {
   components: {
-    Number
+    Number,
+    FontAwesomeIcon
   },
-  props: ['name', 'quantity']
+  props: ['name', 'quantity', 'index']
 }
 </script>
