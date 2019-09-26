defe4a867865cdbbc7a7d4dbe212aa9018cf9c23
Scott97 <scottbarnard5@gmail.com>
2019-09-26 13:59:45 +1000

    Cleaned up code of DisplaySalesRecords page

---
+++ components/TimeSelectionButtons.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-button @click="$emit('today')">
+      Today
+    </b-button>
+    <b-button @click="$emit('week')">
+      Past Week
+    </b-button>
+    <b-button @click="$emit('month')">
+      Past Month
+    </b-button>
+    <b-button @click="$emit('year')">
+      Past Year
+    </b-button>
+    </b>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  padding-top: 15px;
+   justify-content: center;
+  align-items: center;
+  text-align: center;
+   margin-bottom: 12px; /* Add some space below the input */
+}
+</style>
+
+<script>
+// export default {
+//   props: {
+//
+//   }
+// }
+</script>
--- pages/DisplaySalesRecords.vue
+++ pages/DisplaySalesRecords.vue
@@ -1,22 +1,31 @@
-<script src="https://unpkg.com/vue@2.5.17/dist/vue.min.js"></script>
-<script src="https://unpkg.com/babel-polyfill@latest/dist/polyfill.min.js"></script>
-<script src="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.js"></script>
-
 <template>
   <div>
+    <!-- Modals -->
+    <b-modal ref="confirm-delete" title="Confirm Delete">
+      Are you sure you would like to delete this item?
+      <div slot="modal-footer">
+        <b-button variant="danger">
+          Delete
+        </b-button>
+        <b-button variant="outline-dark" @click="$refs['confirm-delete'].hide()">
+          Cancel
+        </b-button>
+      </div>
+    </b-modal>
+
+    <!-- Info modal -->
+    <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
+      <template v-slot:default="{ hide }">
+        <p>{{ infoModal.content[1] }}</p>
+      </template>
+
+      <pre>{{ infoModal.content }}</pre>
+    </b-modal>
+
+    <!-- Page -->
     <navbar title="Display Sales Record" />
-    <div class="links">
-      <b-button v-model="today" @click="DateFiltered">
-        Today
-      </b-button>
-      <b-button>
-        Past Week
-      </b-button>
-      <b-button>
-        Past year
-      </b-button>
-      </b>
-    </div>
+
+    <time-selection-buttons @today="filterByDate('today')" @week="filterByDate('week')" @month="filterByDate('month')" @year="filterByDate('year')" />
 
     <b-container fluid>
       <!-- User Interface controls -->
@@ -75,40 +84,6 @@
           </b-form-group>
         </b-col>
 
-        <!--    <b-col sm="5" md="6" class="my-1">
-          <b-form-group
-            label="Per pprice"
-            label-cols-sm="6"
-            label-cols-md="4"
-            label-cols-lg="3"
-            label-align-sm="right"
-            label-size="sm"
-            label-for="perPpriceSelect"
-            class="mb-0"
-          >
-            <b-form-select
-              id="perPpriceSelect"
-              v-model="perPprice"
-              size="sm"
-              :options="ppriceOptions"
-            />
-          </b-form-group>
-        </b-col>
-
-        <b-col sm="7" md="6" class="my-1">
-          <b-pagination
-            v-model="currentPprice"
-            :total-rows="totalRows"
-            :per-p-price="perPprice"
-            align="fill"
-            size="sm"
-            class="my-0"
-          />
-        </b-col>
-      </b-row> -->
-
-        <!-- Main table element -->
-
         <!-- Main table element -->
         <b-table
           show-empty
@@ -134,7 +109,8 @@
             <b-button size="sm" class="mr-1" @click="info(row.item, row.item, $event.target)">
               Edit
             </b-button>
-            <b-button size="sm" @click="row.toggleDetails">
+            <!-- <b-button size="sm" @click="row.toggleDetails"> -->
+            <b-button size="sm" @click="$refs['confirm-delete'].show()">
               Delete
             </b-button>
           </template>
@@ -150,123 +126,113 @@
           </template>
         </b-table>
 
-        <!-- Info modal -->
-        <b-modal :id="infoModal.id" :title="infoModal.title" ok-only @hide="resetInfoModal">
-          <template v-slot:default="{ hide }">
-            <p>{{ infoModal.content[1] }}</p>
-          </template>
-
-          <pre>{{ infoModal.content }}</pre>
-          <!-- <b-row class="pb-1 pt-1 align-items-center">
-          <b-col cols="4">
-            {{ price }} x {{ Items }}
-          </b-col>
-          <b-col cols="4">
-            <number v-model="price" @input="$emit('update-qty',price)" />
-          </b-col>
-          <b-col cols="4" class="text-right">
-            <b-button variant="outline-danger" class="w-100 text-nowrap" @click="$emit('deleteItem',index)">
-              <font-awesome-icon icon="trash-alt" /> Remove
-            </b-button>
-          </b-col>
-        </b-row>   -->
-        </b-modal>
         </b-table>
       </b-row>
     </b-container>
-    <h2>{{ today }}</h2>
   </div>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar.vue'
-  export default {
-      components: {
-    Navbar
-  },
-    data() {
-      return {
-        items: [
-          {TransactionNo: '1',  price: {cost:40}, NoItems: 6, time: '23/09/2019 : 11:05' },
-          {TransactionNo: '2',  price: {cost:40}, NoItems: 4, time: '23/09/2019 : 12:06' },
-          {TransactionNo: '3', price: {cost:40},NoItems: 1, time: '23/09/2019 : 12:06'},
-          {TransactionNo: '4',  price: {cost:40}, NoItems: 1, time: '20/09/2019 : 12:06' },
-          {TransactionNo: '5',  price: {cost:40}, NoItems: 1, time: '20/09/2019 : 12:06' },
-          {TransactionNo: '6',  price: {cost:40}, NoItems: 7, time: '23/09/2019 : 12:06' },
-          {TransactionNo: '7',  price: {cost:40}, NoItems: 8, time: '21/09/2019 : 12:06' },
-          {TransactionNo: '8',price: {cost:40}, NoItems: 99, time: '23/09/2019 : 12:06' },
-          {TransactionNo: '9',  price: {cost:40}, NoItems: 100, time: '23/09/2019 : 12:06' },
-          {TransactionNo: '10', price: {cost:40}, NoItems: 5000, time: '22/09/2019 : 12:06' },
-          {TransactionNo: '11',  price: {cost:40}, NoItems: 69, time: '21/09/2019 : 12:06' },
-          {TransactionNo: '12',  price: {cost:40}, NoItems: 59, time: '21/09/2019 : 12:06', }
-        ],
-        fields: [
-          { key: 'TransactionNo', label: 'Transaction Code'},
-          { key: 'time', label: 'Time of Sale', sortable: true, class: 'text-center' },
-          { key: 'NoItems', label: 'No of Items', sortable: true, sortDirection: 'desc' },
-          { key: 'price', label: 'Cost of Sale', sortable: true, sortDirection: 'desc' },
-          { key: 'actions', label: 'Actions' }
-        ],
-        totalRows: 1,
-        currentPprice: 1,
-        perPprice: 5,
-        ppriceOptions: [5, 10, 15],
-        sortBy: '',
-        sortDesc: false,
-        sortDirection: 'asc',
-        filter: null,
-        filterOn: [],
-        today: '',
-        infoModal: {
-          id: 'info-modal',
-          title: '',
-          content: '',
-          TransactionNo: '',
-          money: '$',
-        }
-      }
-    },
-    computed: {
-      sortOptions() {
-        // Create an options list from our fields
-        return this.fields
-          .filter(f => f.sortable)
-          .map(f => {
-            return { text: f.label, value: f.key }
-          })
-      }
-    },
-    mounted() {
-      // Set the initial number of items
-      this.totalRows = this.items.length
-    },
-    methods: {
-      info(item, index, button) {
-        this.infoModal.title = item.TransactionNo;
-        this.infoModal.content = item;
-        this.infoModal.TransactionNo = item.TransactionNo;
-        this.$root.$emit('bv::show::modal', this.infoModal.id, button)
-      },
-      resetInfoModal() {
-        this.infoModal.title = ''
-        this.infoModal.content = ''
-      },
-      onFiltered(filteredItems) {
-        // Trigger pagination to update the number of buttons/pprices due to filtering
-        this.totalRows = filteredItems.length
-        this.currentPprice = 1
-      },
-      DateFiltered() {
-        var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-this.today = dd + '/' + mm + '/' + yyyy;
-this.filter = this.today
-      }
+import TimeSelectionButtons from '~/components/TimeSelectionButtons.vue'
 
+export default {
+  components: {
+    Navbar, TimeSelectionButtons
+  },
+  data () {
+    return {
+      items: [
+        { TransactionNo: '1', price: { cost: 40 }, NoItems: 6, time: '23/09/2019 : 11:05' },
+        { TransactionNo: '2', price: { cost: 40 }, NoItems: 4, time: '23/09/2019 : 12:06' },
+        { TransactionNo: '3', price: { cost: 40 }, NoItems: 1, time: '23/09/2019 : 12:06' },
+        { TransactionNo: '4', price: { cost: 40 }, NoItems: 1, time: '20/09/2019 : 12:06' },
+        { TransactionNo: '5', price: { cost: 40 }, NoItems: 1, time: '20/09/2019 : 12:06' },
+        { TransactionNo: '6', price: { cost: 40 }, NoItems: 7, time: '23/09/2019 : 12:06' },
+        { TransactionNo: '7', price: { cost: 40 }, NoItems: 8, time: '21/09/2019 : 12:06' },
+        { TransactionNo: '8', price: { cost: 40 }, NoItems: 99, time: '23/09/2019 : 12:06' },
+        { TransactionNo: '9', price: { cost: 40 }, NoItems: 100, time: '23/09/2019 : 12:06' },
+        { TransactionNo: '10', price: { cost: 40 }, NoItems: 5000, time: '22/09/2019 : 12:06' },
+        { TransactionNo: '11', price: { cost: 40 }, NoItems: 69, time: '21/09/2019 : 12:06' },
+        { TransactionNo: '12', price: { cost: 40 }, NoItems: 59, time: '21/09/2019 : 12:06' }
+      ],
+      fields: [
+        { key: 'TransactionNo', label: 'Transaction Code' },
+        { key: 'time', label: 'Time of Sale', sortable: true, class: 'text-center' },
+        { key: 'NoItems', label: 'No of Items', sortable: true, sortDirection: 'desc' },
+        { key: 'price', label: 'Cost of Sale', sortable: true, sortDirection: 'desc' },
+        { key: 'actions', label: 'Actions' }
+      ],
+      totalRows: 1,
+      currentPprice: 1,
+      perPprice: 5,
+      ppriceOptions: [5, 10, 15],
+      sortBy: '',
+      sortDesc: false,
+      sortDirection: 'asc',
+      filter: null,
+      filterOn: [],
+      today: '',
+      infoModal: {
+        id: 'info-modal',
+        title: '',
+        content: '',
+        TransactionNo: '',
+        money: '$'
+      }
     }
+  },
+  computed: {
+    sortOptions () {
+      // Create an options list from our fields
+      return this.fields
+        .filter(f => f.sortable)
+        .map((f) => {
+          return { text: f.label, value: f.key }
+        })
+    }
+  },
+  mounted () {
+    // Set the initial number of items
+    this.totalRows = this.items.length
+  },
+  methods: {
+    info (item, index, button) {
+      this.infoModal.title = item.TransactionNo
+      this.infoModal.content = item
+      this.infoModal.TransactionNo = item.TransactionNo
+      this.$root.$emit('bv::show::modal', this.infoModal.id, button)
+    },
+    resetInfoModal () {
+      this.infoModal.title = ''
+      this.infoModal.content = ''
+    },
+    onFiltered (filteredItems) {
+      // Trigger pagination to update the number of buttons/pprices due to filtering
+      this.totalRows = filteredItems.length
+      this.currentPprice = 1
+    },
+    filterByDate (option) {
+      // Get Date
+      const today = new Date()
+      const dd = String(today.getDate()).padStart(2, '0')
+      const mm = String(today.getMonth() + 1).padStart(2, '0')
+      const yyyy = today.getFullYear()
+
+      // Do Filtering
+      if (option === 'today') {
+        this.filter = dd + '/' + mm + '/' + yyyy
+      } else if (option === 'week') {
+        // Do Something
+      } else if (option === 'month') {
+        this.filter = mm + '/' + yyyy
+      } else if (option === 'year') {
+        this.filter = yyyy
+      }
+    }
+
   }
+}
 </script>
 
 <style>
@@ -288,16 +254,9 @@ this.filter = this.today
   align-items: center;
   text-align: center;
 }
-.links {
-  padding-top: 15px;
-   justify-content: center;
-  align-items: center;
-  text-align: center;
-   margin-bottom: 12px; /* Add some space below the input */
-}
 .table {
     border-collapse: collapse; /* Collapse borders */
-  border: 1px solid #ddd; /* Add a grey border */
+    border: 1px solid #ddd; /* Add a grey border */
     align-items: center;
     text-align: center;
     justify-content: center;
