283f188142ce4e24ff2be7e2886b0bf9668e33c1
Scott97 <scottbarnard5@gmail.com>
2019-10-13 21:33:44 +1100

    Changed home page

--- pages/index.vue
+++ pages/index.vue
@@ -12,23 +12,31 @@
           </h2>
         </b-col>
       </b-row>
-      <b-row>
-        <b-col>
+      <b-row class="pb-4">
+        <b-col cols="4">
           <b-button href="/addsalesrecord" variant="outline-primary" class="w-100">
             <font-awesome-icon icon="plus" class="w-100 mt-4 mb-4 big-icon" />
             Add Sales Record
           </b-button>
         </b-col>
-        <b-col>
+        <b-col cols="4">
           <b-button href="/displaysalesrecords" variant="outline-dark" class="w-100">
             <font-awesome-icon icon="receipt" class="w-100 mt-4 mb-4 big-icon" />
             Display Sales Records
           </b-button>
         </b-col>
-        <b-col>
-          <b-button href="/displaystock" variant="outline-primary" class="w-100">
+        <b-col cols="4">
+          <b-button href="/displaystock" variant="outline-info" class="w-100">
             <font-awesome-icon icon="capsules" class="w-100 mt-4 mb-4 big-icon" />
             Display Stock
+          </b-button>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="4">
+          <b-button href="/salesstatistics" variant="outline-danger" class="w-100">
+            <font-awesome-icon icon="chart-line" class="w-100 mt-4 mb-4 big-icon" />
+            Sales Statistics
           </b-button>
         </b-col>
       </b-row>
@@ -37,14 +45,12 @@
 </template>
 
 <script>
-import { faPlus, faCapsules, faReceipt } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faCapsules, faReceipt, faChartLine } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import Navbar from '~/components/Navbar.vue'
 
-library.add(faPlus)
-library.add(faCapsules)
-library.add(faReceipt)
+library.add(faPlus, faCapsules, faReceipt, faChartLine)
 
 export default {
   components: {
