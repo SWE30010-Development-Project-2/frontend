ca6737c26236354ad76d2992d50db7ec3d458244
Scott97 <scottbarnard5@gmail.com>
2019-10-15 21:23:37 +1100

    Added some nice pics of the drugs

--- pages/debug.vue
+++ pages/debug.vue
@@ -31,13 +31,12 @@ export default {
       products: [
         { name: 'Paracetamol', barcode: '931001244534', price: 6.00, description: 'Cheap Panadol' },
         { name: 'Sambucol', barcode: '124499953403', price: 3.00, description: 'Medicine of some type' },
-        { name: 'Xanax', barcode: '124454444333', price: 20.00, description: 'A drug named after the rapper Lil Xan' },
+        { name: 'Xanax', barcode: '124454444333', price: 20.00, description: 'Some medicine' },
         { name: 'Glucophage', barcode: '454943112345', price: 20.00, description: 'Medicine of some type' },
         { name: 'Amoxil', barcode: '000000000112', price: 40.00, description: 'Medicine of some type' },
         { name: 'Lipitor', barcode: '000333300112', price: 30.00, description: 'Medicine of some type' },
         { name: 'Zofran', barcode: '069693808812', price: 2.00, description: 'Medicine of some type' },
-        { name: 'Panadol', barcode: '777788880000', price: 8.50, description: 'Fancy Paracetemol' },
-        { name: 'Vicodin', barcode: '555500226688', price: 16.00, description: 'Medicine of some type' },
+        { name: 'Panadol', barcode: '777788880000', price: 8.50, description: 'Paracetemol' },
         { name: 'Neurontin', barcode: '556600225656', price: 12.50, description: 'Medicine of some type' }
       ]
     }
