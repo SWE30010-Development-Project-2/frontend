9455a2cd543e91a11c8cf1481ad2e54b26353ddd
Scott97 <scottbarnard5@gmail.com>
2019-10-20 15:41:58 +1100

    Bugfix for graphs

--- components/Graph.vue
+++ components/Graph.vue
@@ -38,23 +38,16 @@ export default {
       return { labels, datasets }
     }
   },
+  watch: {
+    chartdata (data) {
+      this.renderChart(data, this.options)
+    }
+  },
   mounted () {
     this.renderChart(this.chartdata, this.options)
   },
   methods: {
-    ...Formatting,
-    stringToColour (str) {
-      let hash = 0
-      for (let i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash)
-      }
-      let colour = '#'
-      for (let j = 0; j < 3; j++) {
-        const value = (hash >> (j * 8)) & 0xFF
-        colour += ('00' + value.toString(16)).substr(-2)
-      }
-      return colour
-    }
+    ...Formatting
   }
 }
 </script>
